654c2cdd435d1163ff639018fbb6c6b4e923b4fa
hjanjua90 <harpreet_mann23@yahoo.com>
2022-04-20 18:17:35 -0700

    Adding Job Model

---
+++ server/models/job.model.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
+
+const JobSchema = new mongoose.Schema({
+    // jobTitle, validation 1-30 characters, text
+    title:{
+        type:String,
+        required:[true, "Job Title is required"],
+        maxlength:[30, "Job title cannot be more than 30 characters"]
+    },
+    // CompanyName validation 1-50 characters, text
+    name:{
+        type:String,
+        required:[true, "Job Title is required"],
+        maxlength:[50, "Job title cannot be more than 50 characters"]
+    },
+    // Salary (number) validation not 0
+    salary:{
+        type:Number,
+        required:[true, "Salary amount is required"],
+        min:0,
+    },
+    // Remote,OnSite, Hybrid
+    jobType:{
+        type:String,
+        enum: [
+            "Remote",
+            "On Site",
+            "Hybid"
+        ],
+        required:[true, "Please specify job location."]
+    },
+    // location 
+    location:{
+        type:String,
+    },
+    // nextFollowUp (Calender), date
+    followUp:{
+        type:Date
+    },
+    // additionalNotes, text
+    notes:{
+        type:String,
+    },
+    // stage of interview, enum
+    stage:{
+        type:String,
+        enum:[
+            "Applied",
+            "Pending Company Response",
+            "Technical Interview",
+            "Pending Company Offer",
+            "Pending My Decision", 
+            "Other: See Additional Notes"
+        ]
+    },
+    // Area of expertise
+    skills:{
+        type:String
+    },
+    // notable benefits,
+    benefits:{
+        type:String,
+    },
+    createdBy:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+},{timestamps:true})
+
+const Job = mongoose.model("Job", JobSchema);
+
+// unique validation
+
+JobSchema.plugin(uniqueValidator,{message:"Please enter a unique {VALUE}"})
+
+module.exports = Job;

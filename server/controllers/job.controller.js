cef3d02a0edce5f9b081a51afc9ddd932708ba6b
Harish Sathisan <harish.sathisan@icloud.com>
2022-04-21 01:01:25 -0700

    basic scafolding for Fav Pane & Dashboard added

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -2,139 +2,131 @@ const Job = require("../models/job.model");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
+module.exports = {
+	findAllJobs: (req, res) => {
+		Job.find()
+			.populate("createdBy", "username email")
+			.then((allJobs) => {
+				console.log(allJobs);
+				res.json(allJobs);
+			})
+			.catch((err) => {
+				console.log("finding allJobs failed");
+				res.json({ message: "Something went wrong in findAll", error: err });
+			});
+	},
 
+	createNewJob: (req, res) => {
+		//const newJobObject = new Job(req.body);
+		Job.create(req.body);
+		console
+			.log(req.body)
 
+			//commenting these out to get a response.
+			// const decodedJWT = jwt.decode(req.cookies.userToken,{
+			//     complete:true
+			// })
 
-    module.exports = {
+			// newJobObject.createdBy = decodedJWT.payload.id;
 
+			//newJobObject.save()
 
-        findAllJobs:(req, res)=>{
-            Job.find()
-                .populate("createdBy", "username email")
-                .then((allJobs)=>{
-                    console.log(allJobs);
-                    res.json(allJobs)
-                })
-                .catch((err)=>{
-                    console.log("finding allJobs failed");
-                    res.json({message:"Something went wrong in findAll", error: err})
-                })
+			.then((newJob) => {
+				console.log(newJob);
+				res.json(newJob);
+			})
+			.catch((err) => {
+				console.log("Something went wrong in createNewJob");
+				//We set the response status of 400 to
+				//display our err, which is the rejection of our promise.
 
-        },
+				//A 400 status means our client is talking
+				//to our server just fine, but the client isn't sending good info.
 
-        createNewJob: (req, res)=>{
-            const newJobObject = new Job(req.body);
-            console.log(req.body)
+				//A 404 status error means the client's
+				//request isn't to the right place or your server is not set up properly
 
-            const decodedJWT = jwt.decode(req.cookies.userToken,{
-                complete:true
-            })
+				// a status of 200 means we are looking good!
+				res.status(400).json(err);
+			});
+	},
 
-            newJobObject.createdBy = decodedJWT.payload.id;
+	findOneJob: (req, res) => {
+		//We use the paramater's (params) or the client's request to search for a
+		//specific document by the field (here _id) specified
+		Job.findOne({ _id: req.params.id }) //the params id MUST MATCH how we write it in our routes!!!
+			.then((oneJob) => {
+				console.log(oneJob);
+				res.json(oneJob);
+			})
+			.catch((err) => {
+				console.log("Find One Job failed");
+				res.json({ message: "Something went wrong in findOneJob", error: err });
+			});
+	},
 
-            newJobObject.save()
-            
-                .then((newJob)=>{
-                    console.log(newJob);
-                    res.json(newJob);
-                })
-                .catch((err)=>{
-                    console.log("Something went wrong in createNewJob");
-                    //We set the response status of 400 to 
-                    //display our err, which is the rejection of our promise.
+	deleteOneJob: (req, res) => {
+		Job.deleteOne({ _id: req.params.id })
+			.then((deletedJob) => {
+				console.log(deletedJob);
+				res.json(deletedJob);
+			})
+			.catch((err) => {
+				console.log("delete One Job failed");
+				res.json({
+					message: "Something went wrong in deleteOneJob",
+					error: err,
+				});
+			});
+	},
 
-                //A 400 status means our client is talking 
-                    //to our server just fine, but the client isn't sending good info.
+	updateJob: (req, res) => {
+		//This Mongoose query requires both a parameter AND body from the request!
+		Job.findOneAndUpdate(
+			{ _id: req.params.id },
+			req.body,
+			//These options return a new doc and allow schema valids to run on PUT req
+			{ new: true, runValidators: true }
+		)
+			.then((updatedJob) => {
+				console.log(updatedJob);
+				res.json(updatedJob);
+			})
+			.catch((err) => {
+				console.log("Something went wrong in updateJob");
+				res.status(400).json(err); //See above (explained in create)
+			});
+	},
+	findAllJobsByUser: (req, res) => {
+		if (req.jwtpayload.username !== req.params.username) {
+			console.log("not the user");
 
-
-                //A 404 status error means the client's 
-                    //request isn't to the right place or your server is not set up properly
-
-                // a status of 200 means we are looking good!
-                    res.status(400).json(err)
-                })
-        },
-
-        findOneJob: (req, res)=>{
-                    //We use the paramater's (params) or the client's request to search for a
-            //specific document by the field (here _id) specified
-            Job.findOne({ _id: req.params.id })//the params id MUST MATCH how we write it in our routes!!!
-                .then((oneJob)=>{
-                    console.log(oneJob);
-                    res.json(oneJob);
-                })
-                .catch((err)=>{
-                    console.log("Find One Job failed");
-                    res.json({ message: "Something went wrong in findOneJob", error: err })
-                })
-        },
-
-        deleteOneJob: (req, res)=>{
-            Job.deleteOne({_id: req.params.id})
-                .then((deletedJob)=>{
-                    console.log(deletedJob);
-                    res.json(deletedJob);
-                })
-                .catch((err)=>{
-                    console.log("delete One Job failed");
-                    res.json({ message: "Something went wrong in deleteOneJob", error: err })
-                })
-        },
-
-
-        updateJob: (req, res)=>{
-            //This Mongoose query requires both a parameter AND body from the request!
-            Job.findOneAndUpdate({_id: req.params.id},
-                req.body,
-                //These options return a new doc and allow schema valids to run on PUT req
-                {new: true, runValidators: true}
-                )
-                .then((updatedJob)=>{
-                    console.log(updatedJob)
-                    res.json(updatedJob)
-                })
-                .catch((err)=>{
-                    console.log("Something went wrong in updateJob");
-                    res.status(400).json(err) //See above (explained in create)
-                })
-        },
-        findAllJobsByUser: (req, res)=>{
-
-            if(req.jwtpayload.username !== req.params.username){
-                console.log("not the user");
-
-                User.findOne({username: req.params.username})
-                    .then((userNotLoggedIn)=>{
-                        Job.find({createdBy: userNotLoggedIn._id})
-                            .populate("createdBy", "username")
-                            .then((allJobsFromUser)=>{
-                                console.log(allJobsFromUser);
-                                res.json(allJobsFromUser);
-                            })
-                    })
-                    .catch((err)=>{
-                        console.log(err);
-                        res.status(400).json(err);
-                    })
-            }
-
-            else{
-                console.log("current user")
-                console.log("req.jwtpayload.id:", req.jwtpayload.id);
-                Job.find({ createdBy: req.jwtpayload.id })
-                    .populate("createdBy", "username")
-                    .then((allJobsFromLoggedInUser) => {
-                        console.log(allJobsFromLoggedInUser);
-                        res.json(allJobsFromLoggedInUser);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        res.status(400).json(err);
-                    })
-            }
-
-        }
-
-
-
-    }
+			User.findOne({ username: req.params.username })
+				.then((userNotLoggedIn) => {
+					Job.find({ createdBy: userNotLoggedIn._id })
+						.populate("createdBy", "username")
+						.then((allJobsFromUser) => {
+							console.log(allJobsFromUser);
+							res.json(allJobsFromUser);
+						});
+				})
+				.catch((err) => {
+					console.log(err);
+					res.status(400).json(err);
+				});
+		} else {
+			console.log("current user");
+			console.log("req.jwtpayload.id:", req.jwtpayload.id);
+			Job.find({ createdBy: req.jwtpayload.id })
+				.populate("createdBy", "username")
+				.then((allJobsFromLoggedInUser) => {
+					console.log(allJobsFromLoggedInUser);
+					res.json(allJobsFromLoggedInUser);
+				})
+				.catch((err) => {
+					console.log(err);
+					res.status(400).json(err);
+				});
+		}
+	},
+};
--- server/models/job.model.js
+++ server/models/job.model.js
@@ -17,7 +17,7 @@ const JobSchema = new mongoose.Schema(
 		},
 		// Salary (number) validation not 0
 		salary: {
-			type: Number,
+			type: String,
 			required: [true, "Salary amount is required"],
 			min: 0,
 		},
@@ -33,7 +33,7 @@ const JobSchema = new mongoose.Schema(
 		},
 		// nextFollowUp (Calender), date
 		followUp: {
-			type: Date,
+			type: String,
 		},
 		// additionalNotes, text
 		notes: {
@@ -59,10 +59,10 @@ const JobSchema = new mongoose.Schema(
 		benefits: {
 			type: String,
 		},
-		createdBy: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-		},
+		// createdBy: {
+		// 	type: mongoose.Schema.Types.ObjectId,
+		// 	ref: "User",
+		// },
 	},
 	{ timestamps: true }
 );
@@ -71,6 +71,6 @@ const Job = mongoose.model("Job", JobSchema);
 
 // unique validation
 
-JobSchema.plugin(uniqueValidator, { message: "Please enter a unique {VALUE}" });
+//JobSchema.plugin(uniqueValidator, { message: "Please enter a unique {VALUE}" });
 
 module.exports = Job;

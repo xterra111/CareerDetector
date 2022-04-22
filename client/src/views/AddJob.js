945b2d1a2efd24a2f856cb9bdf7622c876cabcc4
Lisa <lisa.hl.chen@gmail.com>
2022-04-21 17:51:18 -0700

    Css and AddJob updates

--- client/src/views/AddJob.js
+++ client/src/views/AddJob.js
@@ -15,31 +15,10 @@ const AddJob = () => {
 		//Date Example: https://gist.github.com/mohanramphp/af4f0267f5b1c3c0e726e18019eb2a0b
 	const [notes, setNotes] = useState("");
 	const [stage, setStage] = useState(""); // drop down
-	const [skills, setSkills] = useState("");
-	const [benefits, setBenefits] = useState("");
 	const [createdBy, setCreatedBy] = useState(""); 
 		//is this needed here?
 	const navigate = useNavigate();
     const [errors, setErrors] = useState({});
-
-// Job Title: Text
-// Company: Text
-// Stage of Application:
-	// 	"Applied",
-	// 	"Pending Company Response",
-	// 	"Technical Interview",
-	// 	"Pending Company Offer",
-	// 	"Pending My Decision",
-	// 	"Other: See Additional Notes",
-// - Other: See Additional Notes
-// Salary: Integer
-// Remote:
-// - Unknown
-// - Remote
-// - Hybrid
-// - On-Site
-// Location: Text
-// Additional Notes: Text Area
 
 	const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -52,8 +31,6 @@ const AddJob = () => {
 			followUp,
 			notes,
 			stage,
-			skills,
-			benefits,
 			createdBy
         })
             .then((res) => {
@@ -133,18 +110,18 @@ const AddJob = () => {
 |    | /     \ |    
 '    ''       `'   
 		 --> */}
-			<div class="d-flex justify-content-center align-items-center polaroid-side-display">
+			{/* <div class="d-flex justify-content-center align-items-center polaroid-side-display">
 				<div class="text-center ">
-					{/* <img
+					<img
 						src="../views/img/2011-austria-tunnel-of-windows.jpg"
 						alt="austria-tunnel-of-windows"
 						class="polaroid-sizing"
-					/> */}
+					/>
 					<div class="container">
-						{/* <p>Austria 2011 - L.Chen</p> */}
+						<p>Austria 2011 - L.Chen</p>
 					</div>
 				</div>
-			</div>
+			</div> */}
 
 			{/* <!--
 		.---. .--. .--. .    .
@@ -159,10 +136,12 @@ const AddJob = () => {
 			>				
 				<div className="d-flex justify-content-center">
 					{/* <input type="hidden" value="${userLogin.id}" path="user"/> */}
-					<p className="form-sizing-double card-body">
-						<label> Job Title </label>
+					<div className="form-sizing-double card-body">
+						<label className="form-label"> Job Title </label>
+						<br/>
 						<input
 							type="text"
+							name="jobTitle"
 							onChange={(e) => {
 								setJobTitle(e.target.value)
 							}} />
@@ -171,11 +150,13 @@ const AddJob = () => {
 								<p>{errors.jobTitle.message}</p> :
 								null
 						}
-					</p>
-					<p className="form-sizing-double form-group card-body">
-						<label> Company </label>
+					</div>
+					<div className="form-sizing-double card-body">
+						<label className="form-label"> Company </label>
+						<br/>
 						<input
 							type="text"
+							name="company"
 							onChange={(e) => {
 								setCompany(e.target.value)
 							}} />
@@ -184,11 +165,15 @@ const AddJob = () => {
 								<p>{errors.company.message}</p> :
 								null
 						}
-					</p>
-					<p className="form-sizing-double form-group card-body">
-						<label> Salary </label>
+					</div>
+				</div>
+				<div className="d-flex justify-content-center">
+					<div className="form-sizing-double card-body">
+						<label className="form-label"> Salary </label>
+						<br/>
 						<input
-							type="text"
+							type="number"
+							name="salary"
 							onChange={(e) => {
 								setSalary(e.target.value)
 							}} />
@@ -197,13 +182,13 @@ const AddJob = () => {
 								<p>{errors.salary.message}</p> :
 								null
 						}
-					</p>					
-				</div>
-				<div className="d-flex justify-content-center">
-					<p className="form-sizing-double form-group card-body">
-						<label> Job Type </label>
+					</div>
+					<div className="form-sizing-double card-body">
+						<label className="form-label"> Job Type </label>
+						<br/>
 						<input
 							type="text"
+							name="jobType"
 							onChange={(e) => {
 								setJobType(e.target.value)
 							}} />
@@ -212,11 +197,15 @@ const AddJob = () => {
 								<p>{errors.jobType.message}</p> :
 								null
 						}
-					</p>
-					<p className="form-sizing-double form-group card-body">
-						<label> Location </label>
+					</div>
+				</div>
+				<div className="d-flex justify-content-center">
+					<div className="form-sizing-double card-body">
+						<label className="form-label"> Location </label>
+						<br/>
 						<input
 							type="text"
+							name="location"
 							onChange={(e) => {
 								setLocation(e.target.value)
 							}} />
@@ -225,11 +214,13 @@ const AddJob = () => {
 								<p>{errors.location.message}</p> :
 								null
 						}
-					</p>
-					<p className="form-sizing-double form-group card-body">
-						<label> Next Follow Up </label>
+					</div>
+					<div className="form-sizing-double card-body">
+						<label className="form-label"> Next Follow Up </label>
+						<br/>
 						<input
-							type="text"
+							type="date"
+							name="followUp"
 							onChange={(e) => {
 								setFollowUp(e.target.value)
 							}} />
@@ -238,150 +229,63 @@ const AddJob = () => {
 								<p>{errors.followUp.message}</p> :
 								null
 						}
-					</p>					
-				</div>
-
-			</form>
-
-
-			<form>
-				<div class="d-flex justify-content-center">
-					{/* <!-- userId --> */}
-					{/* <form:input type="hidden" value="${userLogin.id}" path="user" /> */}
-
-					<form input type="hidden" value="" path="user" />
-
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="stageOfInterview">Stage of Interview:</form:label> */}
-
-						<form label="stageOfInterview">Stage of Interview:</form>
-						{/* <form:errors class="text-danger" path="stageOfInterview" /> */}
-						<form input class="form-control" path="stageOfInterview" />
-					</div>
-
-					{/* <div class="form-sizing-double form-group card-body">
-						<form:label path="nextFollowUp">
-							Next Follow Up (Optional):
-						</form:label>
-						<form:errors class="text-danger" path="nextFollowUp" />
-						<form:input type="date" class="form-control" path="nextFollowUp" />
-          </div> */}
-				</div>
-				<div class="d-flex justify-content-center">
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="jobTitle">Job Title:</form:label>
-						<form:errors class="text-danger" path="jobTitle" />
-						<form:input class="form-control" path="jobTitle" /> */}
-					</div>
-
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="company">Company:</form:label>
-						<form:errors class="text-danger" path="company" />
-						<form:input class="form-control" path="company" /> */}
 					</div>
 				</div>
-				<div class="d-flex justify-content-center">
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="areaOfExpertise">Area of Expertise:</form:label>
-						<form:errors class="text-danger" path="areaOfExpertise" />
-						<form:input class="form-control" path="areaOfExpertise" /> */}
-					</div>
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="salary">Salary (Optional):</form:label>
-						<form:errors class="text-danger" path="salary" />
-						<form:input type="number" class="form-control" path="salary" /> */}
+				<div className="d-flex justify-content-center">
+					<div className="form-sizing-double card-body">
+						<label className="form-label"> Stage </label>
+						<br/>
+						<input
+							type="text"
+							name="stage"
+							onChange={(e) => {
+								setLocation(e.target.value)
+							}} />
+						{
+							errors.stage ?
+								<p>{errors.stage.message}</p> :
+								null
+						}
+				</div>
+					<div className="form-sizing-double card-body">
+						<label className="form-label"> Additional Notes </label>
+						<br/>
+						<input
+							type="text"
+							name="notes"
+							onChange={(e) => {
+								setNotes(e.target.value)
+							}} />
+						{
+							errors.notes ?
+								<p>{errors.notes.message}</p> :
+								null
+						}
 					</div>
 				</div>
-				<div class="d-flex justify-content-center">
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="dateApplied">Date Applied (Optional):</form:label>
-						<form:errors class="text-danger" path="dateApplied" />
-						<form:input type="date" class="form-control" path="dateApplied" /> */}
-					</div>
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="excitementLevel">Excitement Level:</form:label>
-						<form:errors class="text-danger" path="excitementLevel" />
-						<form:select
-							class="form-control"
-							path="excitementLevel"
-							name="excitementLevel">
-							<option value="0 - No Opinion">0 - No Opinion</option>
-							<option value="1 - Not Excited">1 - Not Excited</option>
-							<option value="2 - Leaning toward no">
-								2 - Leaning toward no
-							</option>
-							<option value="3 - Leaning toward yes">
-								3 - Leaning toward yes
-							</option>
-							<option value="4 - Really excited!">4 - Really excited!</option>
-							<option value="Other: See Additional Notes">
-								Other: See Additional Notes
-							</option>
-						</form:select> */}
-					</div>
 
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="location">Location:</form:label>
-						<form:errors class="text-danger" path="location" />
-						<form:select
-							class="form-sizing-single form-control"
-							path="location"
-							name="location">
-							<option value="unknown">Unknown</option>
-							<option value="inPerson">In Person</option>
-							<option value="hybrid">Hybrid</option>
-							<option value="remote">Remote</option>
-						</form:select> */}
-					</div>
-				</div>
-				<div class="d-flex justify-content-center">
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="contactInformation">
-							Contact Information:
-						</form:label>
-						<form:errors class="text-danger" path="contactInformation" />
-						<br />
-						<form:textarea class="form-control" path="contactInformation" /> */}
-					</div>
-
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="jobDescription">
-							Job Description (Optional):
-						</form:label>
-						<form:errors class="text-danger" path="jobDescription" />
-						<br />
-						<form:textarea class="form-control" path="jobDescription" /> */}
-					</div>
-				</div>
-				<div class="d-flex justify-content-center">
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="notableBenefits">
-							Notable Benefits (Optional):
-						</form:label>
-						<form:errors class="text-danger" path="notableBenefits" />
-						<br />
-						<form:textarea class="form-control" path="notableBenefits" /> */}
-					</div>
-
-					<div class="form-sizing-double form-group card-body">
-						{/* <form:label path="additionalNotes">
-							Additional Notes (Optional):
-						</form:label> */}
-						{/* <form:errors class="text-danger" path="additionalNotes" />
-						<br />
-						<form:textarea class="form-control" path="additionalNotes" /> */}
-					</div>
-				</div>
-				<div class="box-link-style-submit text-center m-2">
-					<input
-						class="btn btn-link-style-submit form-sizing-single form-group"
-						type="submit"
-						value="Submit"
-					/>
-				</div>
 			</form>
 		</div>
 	);
 };
 
 export default AddJob;
+
+// Job Title: Text
+// Company: Text
+// Stage of Application:
+	// 	"Applied",
+	// 	"Pending Company Response",
+	// 	"Technical Interview",
+	// 	"Pending Company Offer",
+	// 	"Pending My Decision",
+	// 	"Other: See Additional Notes",
+// - Other: See Additional Notes
+// Salary: Integer
+// Remote:
+// - Unknown
+// - Remote
+// - Hybrid
+// - On-Site
+// Location: Text
+// Additional Notes: Text Area

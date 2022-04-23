e67e5b6faf485e3570e293b4c8516c4ff4c9b049
hjanjua90 <harpreet_mann23@yahoo.com>
2022-04-23 09:01:19 -0700

    Adding date validation to job model

--- server/models/job.model.js
+++ server/models/job.model.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-
 const JobSchema = new mongoose.Schema(
 	{
 		// jobTitle, validation 1-30 characters, text
 		title: {
 			type: String,
-			required: [true, "is required"],
+			required: [true, "Job Title is required"],
 			maxlength: [30, "Job title cannot be more than 30 characters"],
 		},
 		// CompanyName validation 1-50 characters, text
 		company: {
 			type: String,
-			required: [true, "Company name is required"],
-			maxlength: [50, "Company name cannot be more than 50 characters"],
+			required: [true, "Job Title is required"],
+			maxlength: [50, "Job title cannot be more than 50 characters"],
 		},
 		// Salary (number) validation not 0
 		salary: {
@@ -35,6 +34,12 @@ const JobSchema = new mongoose.Schema(
 		// nextFollowUp (Calender), date
 		followUp: {
 			type: Date,
+			validate: function(input) {
+				/* return true only if the input is a valid date, AND is 
+				greater than or equal to the current date/time */
+				return typeof new Date(input) === 'date' && new Date(input) >= new Date();
+			},
+			message: input => `${input} must be greater than or equal to the current date!`
 		},
 		// additionalNotes, text
 		notes: {
@@ -64,6 +69,7 @@ const JobSchema = new mongoose.Schema(
 const Job = mongoose.model("Job", JobSchema);
 
 // unique validation
-JobSchema.plugin(uniqueValidator, { message: "Please enter a unique {VALUE}" });
+
+//JobSchema.plugin(uniqueValidator, { message: "Please enter a unique {VALUE}" });
 
 module.exports = Job;
